34d3d7fac5c39494c1193c8253bfe9ecf9bbcede
Neha Sharma <NS232@myscc.ca>
2022-03-28 04:20:46 -0400

    commit

--- NehaBooks.DataAccess/Repository/IRepository/ISP_Call.cs
+++ NehaBooks.DataAccess/Repository/IRepository/ISP_Call.cs
@@ -11,9 +11,9 @@ namespace NehaBooks.DataAccess.Repository.IRepository
     {
         T Single<T>(string procedurename, DynamicParameters param = null);
         void Execute(string procedurename, DynamicParameters param = null);
-
         T OneRecord<T>(string procedurename, DynamicParameters param = null);
         IEnumerable<T> List<T>(string procedurename, DynamicParameters param = null);
+
         Tuple<IEnumerable<T1>, IEnumerable<T2>> List<T1, T2>(string procedurename, DynamicParameters param = null);
     }
 }
---
+++ NehaBooks.DataAccess/Repository/SP_Call.cs
@@ -0,0 +1,13 @@
+﻿using NehaBooks.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NehaBooks.DataAccess.Repository
+{
+  public  class SP_Call :ISP_Call
+    {
+    }
+}
