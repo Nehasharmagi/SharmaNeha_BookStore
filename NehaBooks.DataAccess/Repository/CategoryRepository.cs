2287b5c04601467e4dce3004ce41951eda036cce
Neha Sharma <NS232@myscc.ca>
2022-03-28 03:24:53 -0400

    commit Interface

---
+++ NehaBooks.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using NehaBooks.DataAccess.Repository.IRepository;
+using NehaBooks.Models;
+using SharmaNeha_BookStore.DataAccess.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NehaBooks.DataAccess.Repository
+{
+   public class CategoryRepository :Repository<Category>,ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+    }
+}
