842099e25aec250830eeacf3906df5f6c97df92a
Neha Sharma <NS232@myscc.ca>
2022-04-05 04:24:20 -0400

    commit

--- SharmaNeha_BookStore/wwwroot/js/category.js
+++ SharmaNeha_BookStore/wwwroot/js/category.js
@@ -33,25 +33,27 @@ function loadDataTable() {
         ]
     });
 }
+
+
 function Delete(url) {
     swal({
         title: "Are you sure you want to delete?",
         text: "you will not be able to restore the data!",
-        icon: "Waring",
-        button: true,
+        icon: "Warning",
+        buttons: true,
         dangerMode: true
     }).then((willDelete) => {
         if (willDelete) {
-            $ajax({
+            $.ajax({
                 type: "DELETE",
                 url: url,
-                suceess: function (data) {
+                success: function (data) {
                     if (data.success) {
-                        Toastr.success(data.message);
+                        toastr.success(data.message);
                         dataTable.ajax.reload();
                     }
                     else {
-                        Toastr.error(data.message);
+                        toastr.error(data.message);
                     }
                 }
             });
