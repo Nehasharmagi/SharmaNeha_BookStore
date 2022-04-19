c82ebf6234a8bb571bfc19c155e83d85219ac9c3
W0784767 <W0784767@WDSOL0052Z-12>
2022-04-19 10:34:00 -0400

    update the Product.cs

--- NehaBooks.Models/Product.cs
+++ NehaBooks.Models/Product.cs
@@ -24,13 +24,7 @@ namespace NehaBooks.Models
         public double ListPrice { get; set; }
         [Required]
         [Range(1, 10000)]
-        public double Price { get; set; }
-        [Required]
-        [Range(1, 10000)]
-        public double Price50 { get; set; }
-        [Required]
-        [Range(1, 10000)]
-        public double Price100 { get; set; }
+        
         public string ImageUrl { get; set; }
         [Required]
         public int CategoryId { get; set; }
